81d62ceaaf91db301d633e82fe9af5d029d91dfe
stvz21 <126835371+stvz21@users.noreply.github.com>
2023-04-01 10:40:41 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1631,7 +1631,7 @@ async def upload_revista(path,msg,username):
             urls = response_json["url"]
             await bot.send_message(username, f"Archivo Subdido: {namefiles}\nEnlace:\n"+urls)
             links.append(urls)
-            break
+         #   break
         if len(files) == len(links): 
             await msg.edit("Finalizado⬆️❗")
         else:
